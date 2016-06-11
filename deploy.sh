7fb1f2eff037f454ea12602ac271f1d2812cac9c
Tom Rees <zephod@gmail.com>
2016-06-11 20:46:50 +0100

    Still tweaking deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-MESSAGE="[./deploy.sh] $1"
-if [ -z $MESSAGE ] ; then
-  MESSAGE="[./deploy.sh] Pushing at $(date)"
+set -e
+
+MESSAGE=$1
+if [[ -z $MESSAGE ]] ; then
+  MESSAGE="Pushing at $(date)"
 fi
 
 REPO=zephod/tomre.es.git
 
-set -e
-
-rm -rf public
-git subtree add --prefix=public git@github.com:$REPO gh-pages --squash
+# rm -rf public
+# git subtree add --prefix=public git@github.com:$REPO gh-pages --squash
 hugo --theme="hugo-steam-theme"
-git commit -a -m $MESSAGE
+git commit -a -m "[./deploy.sh] $MESSAGE"
 git push origin master
 git subtree push --prefix=public git@github.com:$REPO gh-pages
 

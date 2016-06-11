904292a3d5f105d9eb4667f468886daa89ca00b9
Tom Rees <zephod@gmail.com>
2016-06-11 20:42:02 +0100

    Updating site at Sat 11 Jun 2016 20:42:02 BST

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-DEST_BUCKET=tomre.es
-CONFIG_FILE=s3cfg
+set -e
 
-if [ ! -e $CONFIG_FILE ] ; then
-  echo "I can't see the config file \"$CONFIG_FILE\"."
-  echo "Create a new s3cmd config here, giving it account credentials with just enough power to control the S3 bucket: \"$DEST_BUCKET\"."
-  exit -3
-fi
-
-TMP=$(mktemp -d)
-hugo --theme="hugo-steam-theme" --destination="$TMP"
-
-# http://s3tools.org/usage
-# http://stackoverflow.com/questions/14807702/amazon-s3-static-website-doesnt-serve-css-or-js-files
-# --no-mime-magic because the magic delivers CSS files as text/plain?!
-# (maybe a problem on my mac)
-s3cmd sync \
-  --force \
-  --no-preserve \
-  --exclude=".DS_Store" \
-  --delete-removed \
-  --acl-public \
-  --no-mime-magic \
-  --config=$CONFIG_FILE \
-  $TMP/ \
-  s3://$DEST_BUCKET/
-
-rm -rf $TMP
+rm -rf public
+git subtree add --prefix=public git@github.com:zephod/tomre.es.git gh-pages --squash
+hugo --theme="hugo-steam-theme"

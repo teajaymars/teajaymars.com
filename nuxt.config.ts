8357371bf0b36a39aad3de4a6c479d62aac12316
Tom Rees <zephod@gmail.com>
2023-06-11 16:37:13 +0100

    Add a favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         {
           rel: "preconnect",
